ba611feb67c4cbb4371ecada8add4440ca99c018
VladanStar <vladancupric@gmail.com>
2021-05-18 19:03:34 +0200

    All built in objects method I done all

--- ObjectIntro/built-inObjects/builtInObjects1.js
+++ ObjectIntro/built-inObjects/builtInObjects1.js
@@ -69,7 +69,7 @@ function hasOddNumber(input) {
     return false;
   }
 }
-console.log(hasOddNumber([8, 2, 2, 2, 6]));
+console.log(hasOddNumber([8, 2, 2, 21, 6]));
 
 // 3.b
 // Write a function that counts the number of elements less than the middle element.
@@ -82,7 +82,7 @@ var lessThanMidd = function (arr) {
     return "Error";
   }
   var count = 0;
-  var middle = Math.floor(arr.length / 2);
+  var middle = Math.round(arr.length / 2);
   for (var i = 0; i < arr.length; i++) {
     if (middle > arr[i]) {
       count++;
@@ -93,7 +93,8 @@ var lessThanMidd = function (arr) {
 console.log(lessThanMidd([-1, 8.1, 3, 6, 2.3, 44, 2.11]));
 
 /* ------------------------  4 ---------------------*/
-// Write a function that finds the smallest element of a given array. The function should return an object that contains the smallest value and its last position in the array.
+// Write a function that finds the smallest element of a given array. 
+//The function should return an object that contains the smallest value and its last position in the array.
 // Input: [1, 4, -2, 11, 8, 1, -2, 3]
 // Output:  { minValue: -2, minLastIndex: 6 }
 
@@ -254,7 +255,7 @@ var mostExpansive = function (list) {
   var price = 0;
   for (var i = 0; i < list.length; i++) {
     price += list[i].price; //problem
-   
+
     if (price[i] < list[i].price) {
       price[i] = list[i].price;
     }
@@ -304,7 +305,7 @@ console.log(allCapitals(str));
 
 // 7.zadatak b)
 // Write a function that checks if a given string contains any digits.
-var str1 = "vladan1daph";
+var str1 = "vladan1cupric";
 
 var containDigit = function (s) {
   var regExp = /[0-9]/;
@@ -349,7 +350,8 @@ var belongInterval = function (n) {
 console.log(belongInterval(nmb));
 
 // 7. zadatak e)
-// Write a function named validator that returns an object with properties stringValidator, passwordValidator, colorValidator, and yearValidator referencing the functions from a) to d).
+// Write a function named validator that returns an object with properties stringValidator, 
+//passwordValidator, colorValidator, and yearValidator referencing the functions from a) to d).
 
 function validator(s, a, b, c, d) {
   return (x = {
@@ -364,62 +366,61 @@ console.log(
   validator(str4, allCapitals, containDigit, validHexDec, belongInterval)
 );
 // 8.Write a function that calculates a number of days to your birthday.
-// Input: 25 February 
+// Input: 25 February
 // Output: 5 days
-var bday = new Date ('May 15 2021');
+var bday = new Date("March 11 2021");
 
-var calculate = function(bday) {
-    var x =new Date();
-    var z= bday.getTime()-x.getTime();
-    var d = Math.round(z/1000/3600/24);
+var calculate = function (bday) {
+  var x = new Date();
+  var z = bday.getTime() - x.getTime();
+  var d = Math.round(z / 1000 / 3600 / 24);
   return d;
-}
+};
 console.log(calculate(bday));
 
- 
 //9. Write a function that for a given departure and arrival time calculates the time the trip takes.
 // 	Input: 8:22:13 11:43:22
 // 	Output: 3 hours 21 minutes 8 seconds
 
-var time1 = Date.parse ('16 May 2021 08:22:13');
+var time1 = Date.parse("16 May 2021 08:22:13");
 
-var time2 = Date.parse ('16 May 2021 11:43:22');
-var arrival = function(time1, time2){
-    var milisec = time2 - time1;
-    var sec = milisec /1000;
-    var sec1= sec%60;
-    var min =(sec-sec1)/60;
-    var min1 =((sec-sec1)/60) %60; 
-    var h = (min-min1)/60 ;
-    return h+':'+min1+':'+sec1;
-}
+var time2 = Date.parse("16 May 2021 11:43:22");
+var arrival = function (time1, time2) {
+  var milisec = time2 - time1;
+  var sec = milisec / 1000;
+  var sec1 = sec % 60;
+  var min = (sec - sec1) / 60;
+  var min1 = ((sec - sec1) / 60) % 60;
+  var h = (min - min1) / 60;
+  return h + ":" + min1 + ":" + sec1;
+};
 
 console.log(arrival(time1, time2));
 
-   
-  
-//10. a. Write a constructor function that creates points in the space. Each point in the space has its own x, y, and z coordinate. For example, (3, 5, 1) can the point in the space.
-function Points(x,y,z){
-    this.x=x;
-    this.y=y;
-    this.z=z;
+//10. a. Write a constructor function that creates points in the space. 
+//Each point in the space has its own x, y, and z coordinate.For example, (3, 5, 1) can the point in the space.
+function Points(x, y, z) {
+  this.x = x;
+  this.y = y;
+  this.z = z;
 }
 
-var point1 = new Points (3,5,1);
-var point2 = new Points (4,2,8);
-console.log(point1,point2);
+var point1 = new Points(3, 5, 1);
+var point2 = new Points(4, 2, 8);
+console.log(point1, point2);
 
+//10. b. Write a function that calculates the distance between to points in the space.
 
+var distance = function (p1, p2) {
+  var d = Math.sqrt(
+    Math.pow(p1.x - p2.x, 2) +
+      Math.pow(p1.y - p2.y, 2) +
+      Math.pow(p1.z - p2.z, 2)
+  );
+  return d;
+};
 
-//10. b. Write a function that calculates the distance between to points in the space. 
-
-var distance = function (p1, p2){
-    var d = Math.sqrt(Math.pow(p1.x-p2.x,2)+Math.pow(p1.y-p2.y,2)+Math.pow(p1.z-p2.z,2))
- return d;
-
-}
-
-console.log (distance(point1,point2));
+console.log(distance(point1, point2));
 
 //11. a. Write a function that generates a random integer value between 5 and 20.
 
@@ -429,38 +430,51 @@ var b = 20;
 var a1 = 50;
 var b1 = 100;
 
-var generate = function(n1, n2){
-var max;
-var min;
+var generate = function (n1, n2) {
+  var max;
+  var min;
 
-    if(n1 > n2){
-        max = n1;
-        min = n2;
-    } else {
-        max = n2;
-        min = n1;
-    }
+  if (n1 > n2) {
+    max = n1;
+    min = n2;
+  } else {
+    max = n2;
+    min = n1;
+  }
+  var c = Math.round((max - min) * Math.random() + min);
+  return c;
+};
 
-    var c = Math.round(((max - min) * Math.random()) +min);
+console.log(generate(a, b));
+console.log(generate(a1, b1));
 
-    return c;
-}
+//11. b. Write a function that generates a random integer value between 50 and 100.
+//11. c. Write a function which expects a number and a callback factory function and returns an array of numbers produced by the factory function.
 
-console.log(generate(a,b));
-console.log(generate(a1,b1));
-
-
-//11. b. Write a function that generates a random integer value between 50 and 100. 
-//11. c. Write a function which expects a number and a callback factory function and returns an array of numbers produced by the factory function.   
-
-var arrayOfNum = function(n, func){
-
-    var arr = [];
-
-    for(var i = 0; i < n; i++){
-        arr.push(func(a,b));
-    }
-    return arr;
-}
-
+var arrayOfNum = function (n, func) {
+  var arr = [];
+  for (var i = 0; i < n; i++) {
+    arr.push(func(a, b));
+  }
+  return arr;
+};
 console.log(arrayOfNum(4, generate));
+
+/*12. Write a function that shuffles the elements of a given array.
+
+Input: [3, 6, 11, 2, 9, 1]
+
+Output: [6, 2, 9, 1, 3, 11] (it can be any random permutation of the given array)*/
+// JavaScript Program to shuffle a given array
+
+// A function to print an array
+
+var array = [3, 6, 11, 2, 9, 1];
+function shuffleArray(array) {
+  for (var i = array.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+console.log(shuffleArray(array));

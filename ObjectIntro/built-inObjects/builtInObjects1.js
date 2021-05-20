0691162c204ff21f8fa436fe40c2d66fa95a037b
VladanStar <vladancupric@gmail.com>
2021-05-20 08:24:57 +0200

    BuiltIn Objects methods objasnjene metode i logika

--- ObjectIntro/built-inObjects/builtInObjects1.js
+++ ObjectIntro/built-inObjects/builtInObjects1.js
@@ -455,6 +455,8 @@ var distance = function (p1, p2) {
 console.log(distance(point1, point2));
 
 //11. a. Write a function that generates a random integer value between 5 and 20.
+/* 
+The Math.random() function returns a floating-point, pseudo-random number in the range 0 to less than 1 (inclusive of 0, but not 1) with approximately uniform distribution over that range — which you can then scale to your desired range. The implementation selects the initial seed to the random number generation algorithm; it cannot be chosen or reset by the user.*/
 
 var a = 5;
 var b = 20;
@@ -500,6 +502,9 @@ Output: [6, 2, 9, 1, 3, 11] (it can be any random permutation of the given array
 // JavaScript Program to shuffle a given array
 
 // A function to print an array
+/* 
+The Math.floor() function returns the largest integer less than or equal to a given number.
+*/
 
 var array = [3, 6, 11, 2, 9, 1];
 function shuffleArray(array) {

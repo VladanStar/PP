a5302f45b634e8a43827018a70f437033a671d1f
VladanStar <vladancupric@gmail.com>
2021-05-25 08:35:25 +0200

    Movie Festival BIT 2021

--- OOP/MovieFestival/movieFestival.js
+++ OOP/MovieFestival/movieFestival.js
@@ -1,125 +1,129 @@
-/////////////
-"use strict";
-/////////////
-
 (function () {
-  //////////////////////// GENRE ////////////////////////
+  // Kreiramo konstruktor za Zanr.
+
   function Genre(name) {
     this.name = name;
-
-    this.getGenreData = function () {
-      var genreString = "";
-      genreString += this.name[0];
-      genreString += this.name[this.name.length - 1];
-
-      return genreString.toUpperCase();
+    //UZIMAMO PRVO I ZADNJE SLOVA ZANRA I RADIMO UPPER U METODI
+    this.getData = function () {
+      var firstLetter = this.name.charAt(0);
+      var lastLetter = this.name.charAt(name.length - 1);
+      var result = firstLetter + lastLetter;
+      return result.toUpperCase();
     };
   }
 
-  ///////////////////////// MOVIE /////////////////////////
+  // Kreiramo konstruktor Movie
 
-  function Movie(title, createGenre, length) {
+  function Movie(title, genre, length) {
     this.title = title;
-    this.genre = createGenre;
+    this.genre = genre;
     this.length = length;
-
-    this.getMovieLength = function () {
-      return this.length;
-    };
-
-    this.getMovieData = function () {
-      return (
-        "\t\t\t\t\t" +
-        this.title +
-        ", " +
-        this.length +
-        "min" +
-        ", " +
-        this.genre.getGenreData()
-      );
+    // PRAVIMO NAZIV FILMA SA ZANROM I DUZINOM
+    this.getDataMovie = function () {
+      return this.title + " " + this.genre + " " + this.length + "min, ";
     };
   }
-
-  ///////////////////////// PROGRAM /////////////////////////
+  // Kreiramo konstrutor za Program
 
   function Program(date) {
     this.date = date;
-    this.movies = [];
+    this.moviesList = [];
 
-    this.getNumberOfMovies = function () {
-      return this.movies.length;
+    this.addMovie = function (movie) {
+      this.moviesList.push(movie);
     };
-
-    this.addMovie = function (movies) {
-      this.movies.push(movies);
+    this.getNumOfMovies = function () {
+      return this.moviesList.length;
     };
-
-    this.getProgramData = function () {
-      var movieString = "";
-      var totalMoviesLength = 0;
-      for (var i = 0; i < this.movies.length; i++) {
-        totalMoviesLength += this.movies[i].getMovieLength();
-        movieString += "\n" + this.movies[i].getMovieData();
+    this.programLength = function () {
+      var length = 0;
+      for (var i = 0; i < this.moviesList.length; i++) {
+        var movie = this.moviesList[i];
+        length += movie.length;
       }
-      return this.date + ", " + totalMoviesLength + "min" + ", " + movieString;
+      return length;
+    };
+    this.getData = function () {
+      var myDate = this.date;
+      var dayProgram = "\t";
+      // DNEVNI PROGRAM DAN,MESEC,GODINA SA UKUPNOM DUZINOM POZIVAMO METODU DUZINA FILMA
+      dayProgram +=
+        this.date.getDate() +
+        "." +
+        this.date.getMonth() +
+        "." +
+        this.date.getYear() +
+        "," +
+        " program length from " +
+        this.programLength() +
+        " min  from all movies \n";
+      for (var i = 0; i < this.moviesList.length; i++) {
+        var movie = this.moviesList[i];
+        dayProgram += "\t\t\t" + movie.getDataMovie() + "\n";
+      }
+      return dayProgram;
     };
   }
 
-  ///////////////////////// FESTIVAL /////////////////////////
+  // Kreiramo konstruktor za festival
 
-  function Festival(name) {
-    this.name = name;
-    this.program = [];
-
-    this.addProgram = function (program) {
-      this.program.push(program);
+  function Festival(nameFestival) {
+    this.nameFestival = nameFestival;
+    this.programs = [];
+    // this.totalNumOfMovies = totalNumOfMovies;
+    this.programFestival = function (program1) {
+      return this.programs.push(program1);
     };
+    // IME FESTIVALA I UKUPAN BROJ FILMOVA
+    this.getDataFestival = function () {
+      var final = "";
 
-    this.getFestivalData = function () {
-      var festivalString = "";
-      var festivalArray = [];
-      for (var i = 0; i < this.program.length; i++) {
-        festivalString += this.program[i].getProgramData();
-        festivalArray += this.program[i].getNumberOfMovies();
+      var firstFinal =
+        nameFestival +
+        " has  movie titles " +
+        this.programFestival() +
+        " movies \n";
+
+      var result = "";
+      for (var i = 0; i < this.programs.length - 1; i++) {
+        result += "\t" + this.programs[i];
       }
-
-      return (
-        this.name +
-        " has " +
-        festivalArray +
-        " movie titles" +
-        "\n " +
-        "\t\t" +
-        festivalString
-      );
+      final = firstFinal + result;
+      return final;
     };
   }
 
-  ////////////////////////////////////////////////////////////////
+  /***KREIRAMO INSTANCU ZANRA UBACUJEMO ARGUMENT IMENA ***/
+  var drama = new Genre("Drama");
+  var horror = new Genre("Horror");
+  // console.log(g1.getData());
+  // console.log(g2.getData());
 
-  //Create Genre:
-  var createGenre = new Genre("action");
-  var createGenreTwo = new Genre("comedy");
+  /*** KREIRAMO INSTANCU FILMA UBACUJEMO  NAZIV FILMA, VREDNOST ZANRA. DUZINU U MIN ***/
+  var movie1 = new Movie("Full Metal Jacket", drama.getData(), 116);
+  var movie2 = new Movie("Shuter island", drama.getData(), 130);
+  var movie3 = new Movie(" The Shining", horror.getData(),146);
+  var movie4 = new Movie("Rosemary's Baby", horror.getData(), 136);
+  // console.log(m1.getDataMovie());
+  // console.log(m2.getDataMovie());
 
-  //console.log(createGenre.getGenreData);
+  /*** KREIRAMO INSTANCU PROGRAMA UBACUJEM DATUM U FORMI YYYY, M, DD ***/
+  var program1 = new Program(new Date(2021, 5, 22));
+  var program2 = new Program(new Date(2021, 5, 23));
 
-  //Create Movie:
-  var createMovie = new Movie("The Shawshank Redemption", createGenre, 130);
-  var createMovieTwo = new Movie("Deadpool", createGenreTwo, 108);
+  /** U PROGRAMU POZIVAMO METODU addMovie SA ARGUMENTOM KOJI PROSLEDUJE FILM ***/
+  program1.addMovie(movie1);
+  program1.addMovie(movie2);
+  program1.addMovie(movie3);
+  program2.addMovie(movie1);
+  program2.addMovie(movie2);
+  program2.addMovie(movie4);
 
-  // console.log(createMovie.getMovieData());
+  // console.log(program1.getData());
 
-  //Create Program:
-  var createProgram = new Program("29.10.2010");
-
-  createProgram.addMovie(createMovie);
-  createProgram.addMovie(createMovieTwo);
-
-  //console.log(createProgram.getProgramData());
-
-  //Create Festival:
-  var createFestival = new Festival("Weekend festival");
-  createFestival.addProgram(createProgram);
-
-  console.log(createFestival.getFestivalData());
+  var festival1 = new Festival("Weekend Festival");
+  var festival2 = new Festival("Weekend Festival");
+  festival1.programFestival(program1.getData());
+  festival1.programFestival(program2.getData());
+  console.log(festival1.getDataFestival());
 })();

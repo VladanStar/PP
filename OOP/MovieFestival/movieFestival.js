589d27dee4c961c156103962afa3734c5e1555b1
VladanStar <vladancupric@gmail.com>
2021-05-27 10:35:46 +0200

    New commit festival

--- OOP/MovieFestival/movieFestival.js
+++ OOP/MovieFestival/movieFestival.js
@@ -1,6 +1,6 @@
 (function () {
   console.log("Hi, This is BIT Movie Festival and show mast go on!!!");
-   //Kreiramo konstruktor za Zanr.
+  //Kreiramo konstruktor za Zanr.
  
 
   function Genre(name) {
@@ -30,45 +30,54 @@
   function Program(date) {
     this.date = date;
     this.moviesList = [];
-     this.totalNumberOfMovies = 0;
+    this.totalNumberOfMovies = 0;
 
     this.addMovie = function (movie) {
       this.moviesList.push(movie);
-       this.totalNumberOfMovies++;
     };
+  
    
-    this.getNumOfMovies = function () {
-      return this.moviesList.length;
-    };
-    this.programLength = function () {
-      var length = 0;
-      for (var i = 0; i < this.moviesList.length; i++) {
-        var movie = this.moviesList[i];
-        length += movie.length;
-      }
-      return length;
-    };
-    this.getData = function () {
-      var myDate = this.date;
-      var dayProgram = "\t";
-      // DNEVNI PROGRAM DAN,MESEC,GODINA SA UKUPNOM DUZINOM POZIVAMO METODU DUZINA FILMA
-      dayProgram +=
-        this.date.getDate() +
-        "." +
-        this.date.getMonth() +
-        "." +
-        this.date.getYear() +
-        "," +
-        " program length from " +
-        this.programLength() +
-        " min  from all movies \n";
-      for (var i = 0; i < this.moviesList.length; i++) {
-        var movie = this.moviesList[i];
-        dayProgram += "\t\t\t" + movie.getDataMovie() + "\n";
-      }
-      return dayProgram;
-    };
-  }
+  this.getNumOfMovies = function () {
+    return this.moviesList.length;
+  };
+  this.programLength = function () {
+    var length = 0;
+    for (var i = 0; i < this.moviesList.length; i++) {
+      var movie = this.moviesList[i];
+      length += movie.length;
+    }
+    return length;
+  };
+  this.getData = function () {
+    var myDate = this.date;
+    var dayProgram = "\t";
+    // DNEVNI PROGRAM DAN,MESEC,GODINA SA UKUPNOM DUZINOM POZIVAMO METODU DUZINA FILMA
+    dayProgram +=
+      this.date.getDate() +
+      "." +
+      this.date.getMonth() +
+      "." +
+      this.date.getYear() +
+      "," +
+      " program length from " +
+      this.programLength() +
+      " min  from all movies \n";
+    for (var i = 0; i < this.moviesList.length; i++) {
+      var movie = this.moviesList[i];
+      dayProgram += "\t\t\t" + movie.getDataMovie() + "\n";
+    }
+    return dayProgram;
+  };
+  // this.durationMovie = function () {
+  //     var duration = 0;
+  //     for (var m = 0; m < this.ListOfMovies.length; m++) {
+  //         duration += this.ListOfMovies[i].legnth;
+  //     }
+  //     return duration;
+  // }
+}
+  
+  
 
   // Kreiramo konstruktor za festival
 
